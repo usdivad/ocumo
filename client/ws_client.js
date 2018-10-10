7c98419adb8a5a5d443fdaa63ee34b44d2d52f1e
David Su <david.d.su@gmail.com>
2018-10-10 10:44:58 -0400

    add first few samples, set up code to load based on user index

--- client/ws_client.js
+++ client/ws_client.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     var ws = new WebSocket("ws://localhost:1234");
+    var numSamples = 7;
+    var areSamplesLoaded = false;
+    var samplePlayer;
 
     ws.onopen = function() {
         console.log("ws connected");
@@ -7,7 +10,30 @@ window.onload = function() {
         ws.send("Connected!");
     }
 
-    ws.onmessage = function(e) {
-        console.log("Received: %s", e);
+    ws.onmessage = function(message) {
+        console.log("Received: %s", message);
+        console.log(message);
+
+        var data = JSON.parse(message.data);
+        
+        // Load samples
+        if ("userIdx" in data) {
+            var sampleIdx = (data["userIdx"] % numSamples) + 1;
+            // var sampleName = "sbpl" + (sampleIdx+1);
+            var sampleBasePath = "samples/mp3/";
+            var sampleExt = ".mp3";
+            samplePlayer = new Tone.Players({
+                "sdpl": sampleBasePath + "saron_sdpl" + sampleIdx + sampleExt,
+                "sbpl": sampleBasePath + "saron_sbpl" + sampleIdx + sampleExt
+            }, function() {
+                console.log("Done loading samples!");
+                areSamplesLoaded = true;
+            }).toMaster();
+        }
+
+        // Playback
+        if ("triggerSample" in data && areSamplesLoaded) {
+            samplePlayer.get(data["triggerSample"]).restart();
+        }
     }
 };
--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const app = express();
 const wss = new ws.Server({"port": 1234});
 
 
+var users = {};
+var numUsers = 0;
+
+// Views
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/client/ws_client.html");
 });
 
+// Src
 app.get("/ws_client.js", function(req, res) {
     res.sendFile(__dirname + "/client/ws_client.js");
 });
@@ -17,6 +22,21 @@ app.get("/ws_client.css", function(req, res) {
     res.sendFile(__dirname + "/client/ws_client.css");
 });
 
+// Lib
+app.get("/Tone.js", function(req, res) {
+    res.sendFile(__dirname + "/lib/Tone.min.js");
+});
+
+
+// Media
+app.get("/samples/:sampleFormat/:sampleName", function(req, res) {
+    console.log(req.params);
+    // res.send(req.params);
+    res.sendFile(__dirname + "/samples/" + req.params["sampleFormat"] + "/" + req.params["sampleName"]);
+});
+
+
+// WebSockets
 app.listen(3000, function() {
     console.log("Listning on port 3000");
 });
@@ -25,7 +45,15 @@ app.listen(3000, function() {
 
 
 wss.on("connection", function(ws) {
-    console.log("A user connected!");
+    var userID = Date.now();
+    console.log("User %s connected!", userID);
+
+    users[userID] = ws;
+
+    ws.send(JSON.stringify({"userIdx": numUsers}));
+
+    numUsers++
+
     ws.on("message", function(msg) {
         console.log("Received: %s", msg);
     });
