546b1112eff2513d7d012457c71a81acf1292544
David Su <david.d.su@gmail.com>
2018-10-14 16:42:20 -0400

    comment out old stuff

--- client/sighack_conductor.js
+++ client/sighack_conductor.js
@@ -176,118 +176,118 @@ function stopAll(userID, scheduleTime) {
 // ================================================================
 // OLD STUFF
 
-window.onload = function() {
-    // var ws = new WebSocket("ws://localhost:1234");
-    var ws = new WebSocket(location.origin.replace(/^http/, "ws"));
+// window.onload = function() {
+//     // var ws = new WebSocket("ws://localhost:1234");
+//     var ws = new WebSocket(location.origin.replace(/^http/, "ws"));
 
-    var numUsers = 0; // tODO
-    var numSamples = 6; // for SPD
+//     var numUsers = 0; // tODO
+//     var numSamples = 6; // for SPD
 
-    var midiInput;
-    var shouldTriggerSamples = true;
+//     var midiInput;
+//     var shouldTriggerSamples = true;
 
-    ws.onopen = function() {
-        console.log("ws connected");
+//     ws.onopen = function() {
+//         console.log("ws connected");
 
-        ws.send(JSON.stringify({"connect": "performer"}));
-    }
+//         ws.send(JSON.stringify({"connect": "performer"}));
+//     }
 
-    ws.onmessage = function(message) {
-        var data = JSON.parse(message.data);
-        if ("currSongName" in data) {
-            document.getElementById("currSongNameDiv").innerText = data["currSongName"];
-        }
-    }
+//     ws.onmessage = function(message) {
+//         var data = JSON.parse(message.data);
+//         if ("currSongName" in data) {
+//             document.getElementById("currSongNameDiv").innerText = data["currSongName"];
+//         }
+//     }
 
-    // Load
-    document.getElementById("loadSampleBtn").addEventListener("click", function() {
-        var userID = document.getElementById("loadSampleUserID").value;
-        var samplePath = document.getElementById("loadSamplePath").value;
-        var sampleName = document.getElementById("loadSampleName").value;
+//     // Load
+//     document.getElementById("loadSampleBtn").addEventListener("click", function() {
+//         var userID = document.getElementById("loadSampleUserID").value;
+//         var samplePath = document.getElementById("loadSamplePath").value;
+//         var sampleName = document.getElementById("loadSampleName").value;
 
-        ws.send(JSON.stringify({
-            "loadSample": {
-                "userID": userID,
-                "samplePath": samplePath,
-                "sampleName": sampleName
-            }
-        }));
-    });
+//         ws.send(JSON.stringify({
+//             "loadSample": {
+//                 "userID": userID,
+//                 "samplePath": samplePath,
+//                 "sampleName": sampleName
+//             }
+//         }));
+//     });
 
-    // Trigger samples
+//     // Trigger samples
 
-    // "all"
-    document.getElementById("btnAll").addEventListener("click", function() {
-        ws.send(JSON.stringify({"triggerSample": "all"}));
-    });
+//     // "all"
+//     document.getElementById("btnAll").addEventListener("click", function() {
+//         ws.send(JSON.stringify({"triggerSample": "all"}));
+//     });
 
-    // Indexed
-    for (let i=1; i<numSamples; i++) {
-        document.getElementById("btn" + i).addEventListener("click", function() {
-            ws.send(JSON.stringify({"triggerSample": i}));
-        });
-    }
+//     // Indexed
+//     for (let i=1; i<numSamples; i++) {
+//         document.getElementById("btn" + i).addEventListener("click", function() {
+//             ws.send(JSON.stringify({"triggerSample": i}));
+//         });
+//     }
 
-    // MIDI WOO
-    var handleMIDINoteOn = function(e) {
-        // console.log(e);
-        console.log(e.note.number + "(" + e.note.name + e.note.octave + ")");
+//     // MIDI WOO
+//     var handleMIDINoteOn = function(e) {
+//         // console.log(e);
+//         console.log(e.note.number + "(" + e.note.name + e.note.octave + ")");
 
-        // Hard-coded for SPD setup, MIDI notes 60-68
-        if (e.note.number == 60) {
-            // Go to prev song
-            ws.send(JSON.stringify({"goToSong": "prev"}));
-        }
-        else if (e.note.number == 61) {
-            // // Trigger all samples
-            // ws.send(JSON.stringify({"triggerSample": "all"}));
+//         // Hard-coded for SPD setup, MIDI notes 60-68
+//         if (e.note.number == 60) {
+//             // Go to prev song
+//             ws.send(JSON.stringify({"goToSong": "prev"}));
+//         }
+//         else if (e.note.number == 61) {
+//             // // Trigger all samples
+//             // ws.send(JSON.stringify({"triggerSample": "all"}));
 
-            // Toggle shouldTriggerSamples
-            shouldTriggerSamples = !shouldTriggerSamples;
-            if (shouldTriggerSamples) {
-                document.getElementById("shouldTriggerSamplesDiv").innerText = "shouldTriggerSamples = true";
-            }
-            else {
-                document.getElementById("shouldTriggerSamplesDiv").innerText = "shouldTriggerSamples = false";
-            }
-        }
-        else if (e.note.number == 62) {
-            // Go to next song
-            ws.send(JSON.stringify({"goToSong": "next"}));
-        }
-        else if (e.note.number >= 63 && e.note.number <= 68) {
-            // Trigger individual sample
-            if (shouldTriggerSamples) {
-                ws.send(JSON.stringify({"triggerSample": e.note.number - 62})); // To get 1-6
-            }
-        }
-    };
+//             // Toggle shouldTriggerSamples
+//             shouldTriggerSamples = !shouldTriggerSamples;
+//             if (shouldTriggerSamples) {
+//                 document.getElementById("shouldTriggerSamplesDiv").innerText = "shouldTriggerSamples = true";
+//             }
+//             else {
+//                 document.getElementById("shouldTriggerSamplesDiv").innerText = "shouldTriggerSamples = false";
+//             }
+//         }
+//         else if (e.note.number == 62) {
+//             // Go to next song
+//             ws.send(JSON.stringify({"goToSong": "next"}));
+//         }
+//         else if (e.note.number >= 63 && e.note.number <= 68) {
+//             // Trigger individual sample
+//             if (shouldTriggerSamples) {
+//                 ws.send(JSON.stringify({"triggerSample": e.note.number - 62})); // To get 1-6
+//             }
+//         }
+//     };
 
-    WebMidi.enable(function (err) {
+//     WebMidi.enable(function (err) {
 
-        if (err) {
-            console.log("MIDI ERROR: " + err);
-        }
-        else {
-            console.log("MIDI inputs:");
-            console.log(WebMidi.inputs);
+//         if (err) {
+//             console.log("MIDI ERROR: " + err);
+//         }
+//         else {
+//             console.log("MIDI inputs:");
+//             console.log(WebMidi.inputs);
 
-            midiInput = WebMidi.getInputByName("USB Midi");
+//             midiInput = WebMidi.getInputByName("USB Midi");
 
-            if (midiInput) {
-                midiInput.addListener("noteon", "all", handleMIDINoteOn);
-            }
-            else {
-                console.log("MIDI input not found!! Please re-connect your device");
-            }
-        }
+//             if (midiInput) {
+//                 midiInput.addListener("noteon", "all", handleMIDINoteOn);
+//             }
+//             else {
+//                 console.log("MIDI input not found!! Please re-connect your device");
+//             }
+//         }
 
-    });
+//     });
 
 
-    // Stayin' alive
-    window.setInterval(function() {
-        ws.send(JSON.stringify({"ping": "Stayin' alive"}));
-    }, 30000);
+//     // Stayin' alive
+//     window.setInterval(function() {
+//         ws.send(JSON.stringify({"ping": "Stayin' alive"}));
+//     }, 30000);
 
-};
+// };
